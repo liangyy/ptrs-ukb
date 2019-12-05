62328b4819e006590c4be123cab0ea692d2bc88c
Yanyu Liang <yanyul@uchicago.edu>
2019-12-05 16:56:07 -0600

    added population qc

--- code/rlib_doc.R
+++ code/rlib_doc.R
@@ -15,3 +15,53 @@ meta_info_to_query = function(trait_id, field_id, ninstance, narray) {
 trim_space = function(str) {
   stringr::str_remove_all(str, ' ')
 }
+
+first_non_na = function(vec) {
+  out = NA
+  if(sum(!is.na(vec)) > 0) {
+    out = vec[which(!is.na(vec))[1]]
+  }
+  out
+}
+
+aggregate_instances = function(df, method = first_non_na) {
+  apply(df, 1, first_non_na)
+}
+
+sample_at_most_n = function(n, nmax) {
+  if(n > nmax) {
+    o = 1 : n
+    o = o %in% sample(1 : n, size = nmax, replace = F)
+    return(o)
+  } else {
+    return(rep(TRUE, n))
+  }
+}
+
+subsample_for_vis = function(group, nmax = 10000) {
+  e = data.frame(group = group)
+  e = e %>% group_by(group) %>% mutate(is_selected = sample_at_most_n(n(), nmax = nmax))
+  e %>% pull(is_selected)
+}
+
+myggpairs = function(df, col, ...) {
+  mydat = list()
+  done = c()
+  for(i in colnames(df)) {
+    for(j in colnames(df)) {
+      if(i == j) {
+        next
+      }
+      if(paste(i, j) %in% done) {
+        next
+      }
+      tmp = df[, c(i, j)]
+      colnames(tmp) = c('x', 'y')
+      mydat[[length(mydat) + 1]] = tmp %>% mutate(color = col, xfacet = i, yfacet = j)
+      done = c(done, paste(i, j), paste(j, i))
+    }
+  }
+  p = do.call(rbind, mydat) %>% ggplot() + geom_point(aes(x = x, y = y, color = color), ...) + facet_grid(xfacet ~ yfacet)
+  p
+}
+
