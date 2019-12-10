96895f6b7b619fc731eff9f9db0988f93920caed
Yanyu Liang <yanyul@uchicago.edu>
2019-12-10 15:18:26 -0600

    minor update

--- scripts/construct_phenotype_table/sanity_check.R
+++ scripts/construct_phenotype_table/sanity_check.R
@@ -1,3 +1,6 @@
+# This file is for manual and dirty check
+# not really used for anything serious
+
 library(data.table)
 library(dplyr)
 tmp1 = fread('output/query_first_attempt_with_qc.csv', header = T, sep = ',', data.table = F)
@@ -6,22 +9,26 @@ cols = colnames(tmp1)[c(1, 15:32)]
 tmp1 = tmp1[, cols]
 tmp2 = tmp2[, cols]
 # sum(tmp1 != tmp2)
-both_eid = intersect(tmp1$eid, tmp2$eid)
-tmp1_not_in_tmp2 = tmp1$eid[which(!tmp1$eid %in% tmp2$eid)]
-tmp2_not_in_tmp1 = tmp2$eid[which(!tmp2$eid %in% tmp1$eid)]
-tmp1 = tmp1[match(both_eid, tmp1$eid), ]
-tmp2 = tmp2[match(both_eid, tmp2$eid), ]
+# both_eid = intersect(tmp1$eid, tmp2$eid)
+# tmp1_not_in_tmp2 = tmp1$eid[which(!tmp1$eid %in% tmp2$eid)]
+# tmp2_not_in_tmp1 = tmp2$eid[which(!tmp2$eid %in% tmp1$eid)]
+tmp1 = tmp1[match(tmp2$eid, tmp1$eid), ]
+tmp2 = tmp2[match(tmp1$eid, tmp2$eid), ]
 sum(tmp1 != tmp2)
 
-f1 = readRDS('output/query_first_attempt_with_population_qc.rds')
-f2 = readRDS('output/query_phenotypes_with_population_qc.rds')
-f1$dat_ll %>% filter(eid %in% tmp2_not_in_tmp1)
-f2$dat_ll %>% filter(eid %in% tmp1_not_in_tmp2)
-
+# extra check used for previous tables. 
+# now the tables have been updated.
+# no need anymore
+# 
+# f1 = readRDS('output/query_first_attempt_with_population_qc.rds')
+# f2 = readRDS('output/query_phenotypes_with_population_qc.rds')
+# f1$dat_ll %>% filter(eid %in% tmp2_not_in_tmp1)
+# f2$dat_ll %>% filter(eid %in% tmp1_not_in_tmp2)
+# 
 a1 = fread('~/Desktop/ukbiobank_query_2/query_2_out.csv', header = T, sep = ',', data.table = F)
 a2 = fread('output/query_phenotypes_output.csv', header = T, sep = ',', data.table = F)
 a1$eid[which(!a1$eid %in% a2$eid)]
 a2$eid[which(!a2$eid %in% a1$eid)]
-
-a1 %>% filter(eid %in% tmp2_not_in_tmp1)
-a2 %>% filter(eid %in% tmp1_not_in_tmp2)
+# 
+# a1 %>% filter(eid %in% tmp2_not_in_tmp1)
+# a2 %>% filter(eid %in% tmp1_not_in_tmp2)
