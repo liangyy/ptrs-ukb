b903d13fa9ae6d355d1373d4c164590ac2039571
Yanyu Liang <yanyul@uchicago.edu>
2019-12-19 13:12:13 -0600

    finished running reml using hail on nucleus

--- code/estimate_hsq.py
+++ code/estimate_hsq.py
@@ -71,21 +71,21 @@ if args.inv_norm_predictor == True and args.standardize_predictor == True:
 # read in trait table
 logging.info('Loading trait table')
 filename, trait_colname = parse_input(args.trait_table, 'eid')
-df_trait = tsv_to_pd_df(filename, indiv_col = trait_colname)
-indiv_pool = df_trait[trait_colname].to_list()
+df_trait = ghelper.tsv_to_pd_df(filename, indiv_col = trait_colname)
+indiv_pool = np.array(df_trait[trait_colname].to_list())
 logging.info('--> Current sample size = {}'.format(indiv_pool.shape[0]))
 
 # read in covariate table
-logging.info('Loading trait table')
+logging.info('Loading covariate table')
 filename, covar_colname = parse_input(args.covar_table, 'eid')
-df_covar = tsv_to_pd_df(filename, indiv_col = covar_colname)
+df_covar = ghelper.tsv_to_pd_df(filename, indiv_col = covar_colname)
 indiv_pool = np.intersect1d(indiv_pool, df_covar[covar_colname].to_list())
 logging.info('--> Current sample size = {}'.format(indiv_pool.shape[0]))
 
 # read in predictor matrix
 logging.info('Loading predictor matrix')
-filename, colname = parse_input(args.trait_table, '')
-df_pred_expr = tsv_to_pd_df(filename, indiv_col = colname)
+filename, colname = parse_input(args.predictor_table, '')
+df_pred_expr = ghelper.tsv_to_pd_df(filename, indiv_col = colname)
 df_pred_expr = df_pred_expr.drop(columns = [colname])
 indiv_pool = np.intersect1d(indiv_pool, df_pred_expr.columns.to_list())
 logging.info('--> Current sample size = {}'.format(indiv_pool.shape[0]))
@@ -105,14 +105,14 @@ np_trait = df_trait.to_numpy()
 np_covar = df_covar.to_numpy()
 np_pred_expr = df_pred_expr.to_numpy()
 logging.info('--> Removing constant predictors from matrix. # predictors = {}'.format(np_pred_expr.shape[0]))
-np_pred_expr, _ = ghelp.remove_constant_row(np_pred_expr, np.ones(np_pred_expr.shape[0]))
+np_pred_expr, _ = ghelper.remove_constant_row(np_pred_expr, np.ones(np_pred_expr.shape[0]))
 logging.info('--> After removing constant predictors, # predictors = {}'.format(np_pred_expr.shape[0]))
 if args.inv_norm_predictor == True:
     logging.info('--> Inverse normalizing predictors')
-    np_pred_expr = ghelp.inv_norm_row(np_pred_expr)
+    np_pred_expr = ghelper.inv_norm_row(np_pred_expr)
 if args.standardize_predictor is True:
     logging.info('--> Standardizing predictors')
-    np_pred_expr = ghelp.standardize_row(np_pred_expr)
+    np_pred_expr = ghelper.standardize_row(np_pred_expr)
 if args.with_intercept is True:
     logging.info('--> Adding intercept')
     np_covar = np.append(np.ones([np_covar.shape[0], 1]), np_covar, axis = 1)
@@ -120,8 +120,8 @@ if args.with_intercept is True:
 # loop over traits and fit model
 logging.info('Fitting models')
 logging.info('--> Computing K matrix (Z Z^t)')
-K_mat = np.dot(np_pred_expr.transpose(), np_pred_expr) / np.sqrt(np_pred_expr.shape[0])
-init_nan = np.empty((len(trait_names)))
+K_mat = np.dot(np_pred_expr.transpose(), np_pred_expr) / np_pred_expr.shape[0]
+init_nan = np.empty(len(trait_names))
 init_nan[:] = np.nan
 out = pd.DataFrame({
     'trait': trait_names, 
@@ -132,16 +132,19 @@ out = pd.DataFrame({
 })
 for i in range(len(trait_names)):
     logging.info('--> Working on {}'.format(trait_names[i]))
-    grm, p = hl.stats.LinearMixedModel.from_kinship(
+    grm, _ = hl.stats.LinearMixedModel.from_kinship(
         y = np_trait[:, i],
         x = np_covar,
         k = K_mat
     )
     try:
         grm.fit()
-        out['h_sq'] = grm.h_sq
-        out['h_sq_se'] = grm.h_sq_standard_error
+        out['h_sq'][i] = grm.h_sq
+        out['h_sq_se'][i] = grm.h_sq_standard_error
+    except:
+        pass
 
 # save results
 logging.info('Saving results')
 out.to_csv(args.output, index = None, sep = '\t')
+
