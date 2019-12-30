7a4228ac2f6d4526bd8cc2725d124a25f73c6877
Yanyu Liang <yanyul@uchicago.edu>
2019-12-30 11:02:42 -0600

    debugged

--- submission_scripts/prs_r2/README.md
+++ submission_scripts/prs_r2/README.md
@@ -1,3 +1,11 @@
 For subset1,2,3, PRS of all traits are in one file, so should be run without '{name}' tag.
 
 For other subsets, each file contains PRS for one trait, so should be run with '{name}' tag.
+
+Example run:
+
+```
+OUTDIR=/scratch/t.cri.yliang/PTRS/prs_r2
+bash submit.sh $OUTDIR
+```
+
--- submission_scripts/prs_r2/submit.sh
+++ submission_scripts/prs_r2/submit.sh
@@ -1,3 +1,5 @@
+# ARGS1: OUTDIR
+
 if [[ ! -d logs ]]
 then
   mkdir logs
@@ -5,10 +7,10 @@ fi
 
 for i in `seq 1 3`
 do
-  qsub -v NUM=$i,CONFIG=no_name -N R2-$i run.qsub
+  qsub -v NUM=$i,CONFIG=no_name,OUTDIR=$1 -N R2-$i run.qsub
 done
 
 for i in `seq 4 17`
 do
-  qsub -v NUM=$i,CONFIG=no_name -N R2-$i run.qsub
+  qsub -v NUM=$i,CONFIG=with_name,OUTDIR=$1 -N R2-$i run.qsub
 done
