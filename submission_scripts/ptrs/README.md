f30cfa41de9b3ed4ce424ddd55881a58296b59a5
Yanyu Liang <yanyul@uchicago.edu>
2019-12-31 09:26:36 -0600

    added submission script for mesa ptrs

--- submission_scripts/ptrs/README.md
+++ submission_scripts/ptrs/README.md
@@ -6,6 +6,16 @@ Example:
 indivlist=British-test
 for i in `seq 1 17`
 do 
-  bash submit.sh $i $indivlist-$i
+  bash submit.sh $i $indivlist-$i ctimp
+done
+```
+
+To specify prediction model:
+
+```
+indivlist=British-test
+for i in `seq 1 17`
+do 
+  bash submit.sh $i $indivlist-$i mesa AFA
 done
 ```
--- submission_scripts/ptrs/submit.sh
+++ submission_scripts/ptrs/submit.sh
@@ -1,5 +1,7 @@
 # ARGS1: subset number
 # ARGS2: individual list 
+# ARGS3: configfile middle name
+# ARGS4: (optional) predicted expression model 
 
 NUM=$1
 INDIVLIST=$2
@@ -12,4 +14,9 @@ fi
 traitlist=`cat ../../external_data/martin_et_al_2019ng_table_s6.csv |cut -f 1 -d,|awk '{print tolower($0)}'|tail -n +2`
 spredixcanList=`for i in $traitlist; do echo 'subset'$NUM'_x_'$i; done|tr '\n' ':'|sed 's#:$##g'`
 
-qsub -v INDIVLIST=$INDIVLIST,SPREDIXCANLIST=$spredixcanList,NUM=$NUM -N $NUM-$INDIVLIST run.qsub
+if [[ -z $3 ]]
+then
+  qsub -v INDIVLIST=$INDIVLIST,SPREDIXCANLIST=$spredixcanList,NUM=$NUM,CONFIG=$3 -N $NUM-$INDIVLIST-$3 run.qsub
+else
+  qsub -v INDIVLIST=$INDIVLIST,SPREDIXCANLIST=$spredixcanList,NUM=$NUM,CONFIG=$3,PREDMODEL=$4 -N $NUM-$INDIVLIST-$3-$4 run.qsub
+fi
