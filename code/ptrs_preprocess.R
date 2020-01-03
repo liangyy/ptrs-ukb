acbe22905a46e92daf6f9c63a91fe54d67e7ac87
Yanyu Liang <yanyul@uchicago.edu>
2020-01-03 17:38:40 -0600

    fixing minor thing

--- code/ptrs_preprocess.R
+++ code/ptrs_preprocess.R
@@ -27,9 +27,10 @@ if(opt$mode == 'naive') {
   write.table(df$gene, opt$output, quo = F, col = F, row = F)
 } else if(opt$mode == 'ldblock') {
   annot = read.table('../../output/ld_block_annotation_of_genes.tsv', header = T, sep = '\t')
-  df = df %>% filter(abs(effect_size) < 5) %>% mutate(gene_no_dot = trim_dot(gene))
+  df = df %>% mutate(gene_no_dot = trim_dot(gene))
   df = inner_join(df, annot, by = c('gene_no_dot' = 'gene'))
   df_top = df %>% group_by(ld_block) %>% summarize(top_significance = max(abs(zscore))) %>% ungroup()
   df = df %>% inner_join(df_top, by = 'ld_block') %>% filter(abs(zscore) == top_significance)
+  df = df %>% filter(abs(effect_size) < 5)
   write.table(df$gene, opt$output, quo = F, col = F, row = F)
 }
