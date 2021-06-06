98d2d85260215372057223467a4c99e9441e3d5f
Yanyu Liang <yanyul@uchicago.edu>
2021-06-06 11:03:20 -0500

    finished some runs

--- submission_scripts/calc_grm/calc_grm.sh
+++ submission_scripts/calc_grm/calc_grm.sh
@@ -6,4 +6,6 @@
 
 plink2=/gpfs/data/im-lab/nas40t2/yanyul/softwares/plink2
 
-$plink2 --bgen $1 --sample $2 --make-grm-bin --out $3 --memory $4 --threads $5
+$plink2 --bgen $1 ref-first --sample $2 --make-grm-list --out $3 --memory $4 --threads $5
+
+gzip $3.grm
